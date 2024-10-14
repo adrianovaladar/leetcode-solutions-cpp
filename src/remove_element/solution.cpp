0ccc960818993ffcdc788564e6918124f7074dc1
Adriano Valadar <60299267+adrianovaladar@users.noreply.github.com>
2024-10-14 10:17:07 +0100

    add solution for Remove Element problem

---
+++ src/remove_element/solution.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        nums.erase(std::remove_if(nums.begin(), nums.end(), [=] (int num) { return num == val;}), nums.end());
+        return nums.size();
+    }
+};
