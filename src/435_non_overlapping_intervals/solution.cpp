4829cbe522d04d88bcad25aa45c8aa81a0f9ddc5
Adriano Valadar <60299267+adrianovaladar@users.noreply.github.com>
2025-04-03 01:44:01 +0100

    add solution for Non-overlapping Intervals problem

--- src/435_non_overlapping_intervals/solution.cpp
+++ src/435_non_overlapping_intervals/solution.cpp
@@ -1,1 +1,15 @@
-
+class Solution {
+public:
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        std::sort(intervals.begin(), intervals.end());
+        std::vector<std::vector<int>> vec;
+        for (const auto &interval: intervals) {
+            if (vec.empty() || vec.back().at(1) <= interval.at(0))
+                vec.emplace_back(interval);
+            else {
+                vec.back().at(1) = std::min(interval.at(1), vec.back().at(1));
+            }
+        }
+        return intervals.size() - vec.size();
+    }
+};
